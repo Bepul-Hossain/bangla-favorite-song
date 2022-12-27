4b022d0107353bac4f342ad58c0cd82a9338fdb2
Bepul-Hossain <bepul.cse.ru@gmail.com>
2022-12-27 14:46:02 +0600

    done

--- src/components/Songs.jsx
+++ src/components/Songs.jsx
@@ -2,7 +2,7 @@ import Song from "./Song";
 const Songs = ({ songs, userSearchText }) => {
 
     return (
-        <div>
+        <div className="songs">
             {songs.filter((item) => item.name.toLowerCase().includes(userSearchText.toLowerCase())).map((song, index) =>
                 <Song
                     key={song.name}
